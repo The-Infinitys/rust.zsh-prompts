489b174ceb472ede5e677d1f4282ea9816fc6f24
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-12-29 21:30:48 +0900

    feat: add more icon supports

--- src/modules/os.rs
+++ src/modules/os.rs
@@ -3,16 +3,82 @@ use os_info::Type;
 
 pub fn get_os_icon(color: Option<Color>) -> PromptSegment {
     let info = os_info::get();
+
+    // 共通アイコンの定義
+    let windows = "";
+    let linux = "";
+    let arch = "󰣇";
+    let free_bsd = "";
+    let gentoo = "";
+    let apple = "";
+    let azure = "";
+    let cloud = "";
+    let redhat = "";
+    let fedora = "";
+    let suse = "";
+    let infinity = "";
     let icon = match info.os_type() {
-        Type::Macos => "".to_string(),                 // Apple icon
-        Type::Ubuntu => "".to_string(),                // Ubuntu
-        Type::Fedora => "".to_string(),                // Fedora
-        Type::CentOS | Type::Redhat => "".to_string(), // CentOS/RHEL
-        Type::Arch => "󰣇".to_string(),                  // Arch
-        Type::Debian => "".to_string(),                // Debian
-        Type::Windows => "".to_string(),               // Windows
-        Type::Linux => "".to_string(), // WSL (Linux kernel, but Windows Subsystem) or Generic Linux
-        _ => "󰀵".to_string(),           // Default icon
+        Type::AIX => linux,
+        Type::AlmaLinux => "",
+        Type::Alpaquita => linux,
+        Type::Alpine => "",
+        Type::ALTLinux => linux,
+        Type::Amazon => "",
+        Type::Android => "",
+        Type::AOSC => "",
+        Type::Arch => arch,
+        Type::Artix => "",
+        Type::Bluefin => linux,
+        Type::CachyOS => arch,
+        Type::CentOS => redhat,
+        Type::Cygwin => windows,
+        Type::Debian => "",
+        Type::DragonFly => free_bsd,
+        Type::Elementary => "",
+        Type::Emscripten => "🗲",
+        Type::EndeavourOS => "",
+        Type::Fedora => fedora,
+        Type::FreeBSD => free_bsd,
+        Type::Garuda => "",
+        Type::Gentoo => gentoo,
+        Type::HardenedBSD => free_bsd,
+        Type::Illumos => "",
+        Type::InstantOS => arch,
+        Type::Ios => apple,
+        Type::Kali => "",
+        Type::Linux => linux,
+        Type::Mabox => arch,
+        Type::Macos => apple,
+        Type::Manjaro => "",
+        Type::Mariner => azure,
+        Type::MidnightBSD => free_bsd,
+        Type::Mint => "󰣭",
+        Type::NetBSD => free_bsd,
+        Type::NixOS => "",
+        Type::Nobara => "",
+        Type::OpenBSD => "",
+        Type::OpenCloudOS => cloud,
+        Type::openEuler => linux,
+        Type::openSUSE => suse,
+        Type::OracleLinux => "",
+        Type::PikaOS => "󱗆",
+        Type::Pop => "",
+        Type::Raspbian => "",
+        Type::Redhat => redhat,
+        Type::RedHatEnterprise => redhat,
+        Type::Redox => "Ⓡ",
+        Type::RockyLinux => "",
+        Type::Solus => "",
+        Type::SUSE => suse,
+        Type::Ubuntu => "",
+        Type::Ultramarine => fedora,
+        Type::Unknown => infinity,
+        Type::Uos => linux,
+        Type::Void => "",
+        Type::Windows => windows,
+        Type::Zorin => "",
+        _ => infinity,
     };
-    PromptSegment::new_with_color(icon, &color.unwrap_or(Color::White).to_string())
+
+    PromptSegment::new_with_color(icon.to_string(), &color.unwrap_or(Color::White).to_string())
 }
